1b9d4421d7f479e09422d0f21fb5596a51b25c99
sadasal2302 <122780664+sadasal2302@users.noreply.github.com>
2023-01-16 10:31:23 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    paleta_1.move(-1)
+})
+let paleta_1: game.LedSprite = null
+paleta_1 = game.createSprite(2, 4)
+let paleta_2 = game.createSprite(3, 4)
+basic.forever(function () {
+	
+})
